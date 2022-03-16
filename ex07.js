d0872bc6ecd4a2fcf8236f614fe1a2aa1e912c79
Cami <camilacassimiro@gmail.com>
2022-03-16 13:08:02 -0300

    adicionei o exercício 7

---
+++ ex07.js
@@ -0,0 +1,7 @@
+var anoAtual = prompt('Digite o ano vigente')
+var anoNasc = prompt('Digite o seu ano de nascimento')
+
+var idade = anoAtual - anoNasc;
+var idadeReduzida = idade - 1
+
+alert('Sua idade é: ' + idade + ", se não fez niver ainda, sua idade é " + idadeReduzida);
