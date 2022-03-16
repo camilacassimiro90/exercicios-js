613d081799af3ddc5240853ac8350e5873353617
Cami <camilacassimiro@gmail.com>
2022-03-16 13:49:39 -0300

    adicionei o exercício 4

--- ex04.js
+++ ex04.js
@@ -2,3 +2,6 @@ var nome = prompt('Qual seu nome?')
 var idade = prompt('Qual sua idade?')
 var esporte = confirm('Gosta de esportes?')
 console.log(nome);
+console.log(idade);
+console.log(esporte);
+
