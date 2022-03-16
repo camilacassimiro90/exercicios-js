8d645b1e297a49032d46f55f51ca5923766b38c9
Cami <camilacassimiro@gmail.com>
2022-03-16 12:55:51 -0300

    adicionei o exercício 6

---
+++ ex06.js
@@ -0,0 +1,12 @@
+var sobrenome = prompt('Qual seu sobrenome?')
+alert(
+  'Olá, ' +
+    sobrenome +
+    '. Seu sobrenome contém ' +
+    sobrenome.length +
+    ' letras, onde ' +
+    sobrenome.toUpperCase() +
+    ' é em letras maiúsculas, e ' +
+    sobrenome.length +
+    ' é a quantidade desse sobrenome)'
+)
