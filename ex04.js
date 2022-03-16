df47dc5641fcd7550e8adf52948deefb554d4c16
Cami <camilacassimiro@gmail.com>
2022-03-16 12:42:34 -0300

    adicionei o exercício 5

---
+++ ex04.js
@@ -0,0 +1,4 @@
+var nome = prompt('Qual seu nome?')
+var idade = prompt('Qual sua idade?')
+var esporte = confirm('Gosta de esportes?')
+console.log(nome);
---
+++ ex05.js
@@ -0,0 +1,3 @@
+var sobrenome = prompt('Qual seu sobrenome?')
+var nome = prompt('Qual seu nome?')
+alert('O sobrenome é: ' + sobrenome + ' e o nome é: ' + nome)
