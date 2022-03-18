a662e0adfe95a5ed44c20af5a4e801cb50c141bb
Camila Cassimiro <79873793+camilacassimiro90@users.noreply.github.com>
2022-03-18 20:36:08 -0300

    Update ex07.js

--- ex07.js
+++ ex07.js
@@ -4,6 +4,6 @@ var anoAtual = prompt('Digite o ano vigente')
 var anoNasc = prompt('Digite o seu ano de nascimento')
 
 var idade = anoAtual - anoNasc;
-var idadeReduzida = idade - 1
+// var idadeReduzida = idade - 1
 
-alert('Sua idade é: ' + idade + ", se não fez niver ainda, sua idade é " + idadeReduzida);
+alert('Sua idade é: ' + idade + ", se não fez niver ainda, sua idade é " + (idade -1));
