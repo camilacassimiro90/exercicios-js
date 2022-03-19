fb40e716d6b45051b1026f91fbb675202eef6e22
Camila Cassimiro <79873793+camilacassimiro90@users.noreply.github.com>
2022-03-19 19:43:24 -0300

    Update ex08.js

--- ex08.js
+++ ex08.js
@@ -1,15 +1,51 @@
 // Faça 3 prompts ao usuário: no primeiro, pergunte qual o tipo de cálculo ele deseja fazer (“+”, “-”, “*” ou “/”). No segundo, pergunte o primeiro valor que deseja calcular. No terceiro, o segundo valor que deseja calcular. Em seguida, realize o cálculo e exiba na tela um alert contendo o texto “O resultado do seu cálculo é: X”, onde X é o resultado final do cálculo solicitado pelo usuário.
 
-var operação = prompt('Qual tipo de cálculo deseja fazer?')
-var num1 = prompt('Digite um valor')
-var num2 = prompt('Digite outro valor')
-var resultado = operação
-alert(
-  'O resultado do seu cálculo é: ' +
-    num1 +
-    operação +
-    num2 +
-    'onde' +
-    resultado +
-    'é o resultado final do cálculo solicitado pelo usuário.'
-)
+var num1 = Number(prompt('Entre com o 1º valor: '))
+var op = prompt('Entre com a operação: ')
+var num2 = Number(prompt('Entre com o 2º valor: '))
+
+switch (op) {
+  case '+':
+    resultado = num1 + num2
+    alert(
+      'O resultado do seu cálculo é: ' +
+        resultado +
+        ', onde ' +
+        resultado +
+        ' é o resultado final do cálculo solicitado pelo usuário.'
+    )
+    break
+
+  case '-':
+    resultado = num1 - num2
+    alert(
+      'O resultado do seu cálculo é: ' +
+        resultado +
+        ', onde ' +
+        resultado +
+        ' é o resultado final do cálculo solicitado pelo usuário.'
+    )
+    break
+
+  case '/':
+    resultado = num1 / num2
+    alert(
+      'O resultado do seu cálculo é: ' +
+        resultado +
+        ', onde ' +
+        resultado +
+        ' é o resultado final do cálculo solicitado pelo usuário.'
+    )
+    break
+
+  case '*':
+    resultado = num1 * num2
+    alert(
+      'O resultado do seu cálculo é: ' +
+        resultado +
+        ', onde ' +
+        resultado +
+        ' é o resultado final do cálculo solicitado pelo usuário.'
+    )
+    break
+}
